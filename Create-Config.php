82df79641adf084647cc6709088bdefba1b3e82d
Sec7orTeam <sec7or.team.id@gmail.com>
2025-10-31 15:05:29 +0700

    Update and rename Auto-Create-Config-Mining-Nanswap|XNO.php to Create-Config.php

--- Create-Config.php
+++ Create-Config.php
@@ -33,6 +33,12 @@ echo "$cyan [_--$hijau Kosongkan jika tidak ada API key langsung ENTER$cyan _--]
 echo "$kuning [?]$hijau Masukan alamat XNO$merah [=>]$putih ";
 $xno = trim(fgets(STDIN));
 echo "\n";
+echo "$kuning [?]$hijau Masukan server atau url$merah [=>]$putih ";
+$url = trim(fgets(STDIN));
+echo "\n";
+echo "$kuning [?]$hijau Masukan port$merah [=>]$putih ";
+$port = trim(fgets(STDIN));
+echo "\n";
 echo "$kuning [?]$hijau Masukan API key$merah [=>]$putih ";
 $id = trim(fgets(STDIN));
 echo "\n\n";
@@ -41,10 +47,10 @@ sleep(1);
 echo "$cyan           [_--$hijau RESULT$cyan _--]$hijau \n\n";
 echo "$cyan           [_--$hijau Tanpa API key$cyan _--]$hijau \n\n";
 sleep(1);
-echo "cd xmrig-6.22.2 && ./xmrig -o xmrig.nanswap.com:3333 -a rx -k -u $xno -p x -t 64 --cpu-priority=5 --cpu-memory-pool=-1 --huge-pages-jit --randomx-mode=fast --randomx-1gb-pages \n\n";
+echo "cd xmrig-6.22.2 && ./xmrig -o $url:$port -a rx -k -u $xno -p x -t 64 --cpu-priority=5 --cpu-memory-pool=-1 --huge-pages-jit --randomx-mode=fast --randomx-1gb-pages \n\n";
 
 echo "$cyan           [_--$hijau Menggunakan API key$cyan _--]$hijau \n\n";
 
-echo "cd xmrig-6.22.2 && ./xmrig -o xmrig.nanswap.com:3333 -a rx -k -u $xno -p x -t 64 --cpu-priority=5 --cpu-memory-pool=-1 --huge-pages-jit --randomx-mode=fast --randomx-1gb-pages --api-id=$id \n\n";
+echo "cd xmrig-6.22.2 && ./xmrig -o $url:$port -a rx -k -u $xno -p x -t 64 --cpu-priority=5 --cpu-memory-pool=-1 --huge-pages-jit --randomx-mode=fast --randomx-1gb-pages --api-id=$id \n\n";
 
 echo "$biru [-]$ungu Simpan code ini dengan nama run.sh$biru [-] \n\n";
